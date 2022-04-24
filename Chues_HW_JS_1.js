e494969637ebb73b640409f1e107ad412f6b8bc2
DmitryChues <dmitrychues2312@gmail.com>
2022-04-24 17:23:16 +0300

    JS homework

--- Chues_HW_JS_1.js
+++ Chues_HW_JS_1.js
@@ -28,7 +28,7 @@ let age_1 = 10;
 let age_2 = 18;
 let age_3 = 60;
 if (age_1 < age_2) {
-    console.log("You don't have access cause your age is " + age_1 + " It's less then");
+    console.log("You don't have access cause your age is " + age_1 + " It's less then " + age_2);
 } else if (age_1 >= age_2 && age_1 <= age_3) {
     console.log("Welcome!");
 } else if (age_1 > age_3) {
@@ -45,7 +45,7 @@ function checkAge(age) {
     if (isNaN(age)) {
         console.log("Error!")
     } else if (age < age_2) {
-        console.log("You don't have access cause your age is " + age + " It's less then");
+        console.log("You don't have access cause your age is " + age + " It's less then " + age_2);
     } else if (age >= age_2 && age < age_3) {
         console.log("Welcome!");
     } else if (age > age_3) {
